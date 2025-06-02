25f27c04a46a9b69aa207dc0ade3e54894ad52aa
TechSolutionor98 <techsolutionor1@gmail.com>
2025-06-02 16:28:18 +0500

    update product-detials to product with proper slug from wishlist to product page

--- website_frontend/assets/js/main.js
+++ website_frontend/assets/js/main.js
@@ -80,7 +80,7 @@
     let FileName = window.location.pathname.split("/").reverse()[0];
   
     // If we are at the root path ("/" or no file name), keep the activePage class on the Home item
-    if (FileName === "" || FileName === "index.html") {
+    if (FileName === "" || FileName === "index") {
       // Keep the activePage class on the Home link
       selector.find("li.nav-menu__item.has-submenu").eq(0).addClass("activePage");
     } else {
